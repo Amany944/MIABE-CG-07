8be4920885dd5ec94d8b042511e9a6d76d9defe0
Amany944 <mboungouchristamany@gmail.com>
2026-05-01 15:48:18 +0100

    commit of script.js so that when we are down of the web site a,d we clic on menu icon for mobile, we directly switch to the menu with behavior-smooth

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ const menuBtn = document.getElementById("menuBtn");
 const mobileMenu = document.getElementById("mobileMenu");
 
 menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
+
+    const isHidden = mobileMenu.classList.contains("hidden");
+
+    // 🔼 Scroll seulement si on ouvre le menu
+    if (isHidden) {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+
+        // attendre la fin du scroll
+        setTimeout(() => {
+            mobileMenu.classList.remove("hidden");
+        }, 300);
+
+    } else {
+        // fermer directement
+        mobileMenu.classList.add("hidden");
+    }
 });
